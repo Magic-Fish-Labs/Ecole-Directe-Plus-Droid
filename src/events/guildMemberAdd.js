2073c41d647689149e3c27106b5a5ab2bfc941ca
crapi <crapioutch@gmail.com>
2025-10-01 15:56:37 +0200

    added anti bot raid functions

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,12 +1,125 @@
-const { Events, EmbedBuilder } = require("discord.js");
+const { Events, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const fs = require("fs");
 const path = require("path");
 const ctx = new (require("../global/context"))();
 const jsonConfig = require("../../config.json");
 const logger = require("../helpers/logger");
+
+const handleBotThreats = async (member) => {
+    const warnDMBottingEmbedData = JSON.parse(
+        fs.readFileSync(path.join(__dirname, "../embeds/warnDMBotting.json"), "utf8")
+    );
+
+    const warnDMBottingEmbed = new EmbedBuilder()
+        .setTitle(warnDMBottingEmbedData.title)
+        .setDescription(warnDMBottingEmbedData.description)
+        .setColor(warnDMBottingEmbedData.color)
+        .setAuthor({
+            name: warnDMBottingEmbedData.author.name,
+            url: warnDMBottingEmbedData.author.url || "https://www.ecole-directe.plus/",
+            iconURL: warnDMBottingEmbedData.author.icon_url,
+        });
+    
+    const modChannel = member.guild.channels.cache.find(
+        (channel) => channel.id === jsonConfig.mod_channel
+    );
+
+    const modRole = member.guild.roles.cache.find(
+        (role) => role.id === jsonConfig.mod_role
+    );
+
+    const warnModBottingEmbedData = JSON.parse(
+        fs.readFileSync(path.join(__dirname, "../embeds/warnModBotting.json"), "utf8")
+    );
+
+    const warnModBottingEmbed = new EmbedBuilder()
+        .setTitle(warnModBottingEmbedData.title)
+        .setDescription(warnModBottingEmbedData.description
+            .replace("{memberThreat}", `<@${member.user.id}>`)
+        )
+        .setColor(warnModBottingEmbedData.color)
+        .setAuthor({
+            name: warnModBottingEmbedData.author.name,
+            url: warnModBottingEmbedData.author.url || "https://www.ecole-directe.plus/",
+        });
+
+    const actions = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId('unmute')
+                .setStyle(ButtonStyle.Success)
+                .setLabel('Unmute'),
+
+            new ButtonBuilder()
+                .setCustomId('ban')
+                .setStyle(ButtonStyle.Danger)
+                .setLabel('Ban')
+        );
+
+    if (member.user.bot) {
+        const rolesToRemove = member.roles.cache.filter(role => role.name !== "@everyone");
+        for (const role of rolesToRemove.values()) {
+            await member.roles.remove(role);
+        }
+
+        let mutedRole = member.guild.roles.cache.find(role => role.name === "Muted");
+        if (!mutedRole) {
+            mutedRole = await member.guild.roles.create({
+                name: "Muted",
+                color: "#808080",
+                permissions: [],
+                reason: "Permanent mute for bot threat",
+            });
+
+            for (const channel of member.guild.channels.cache.values()) {
+                if (channel.isTextBased()) {
+                    await channel.permissionOverwrites.edit(mutedRole, {
+                        SendMessages: false,
+                        AddReactions: false,
+                        Speak: false,
+                        Connect: false,
+                    });
+                }
+            }
+        }
+
+        await member.roles.add(mutedRole);
+
+        await member.send({
+            embeds: [warnDMBottingEmbed],
+            content: `|| <@${member.user.id}> ||`,
+        });
+
+        if (modChannel) {
+            const sentMsg = await modChannel.send({
+                embeds: [warnModBottingEmbed],
+                content: `|| <@${modRole}> ||`,
+                components: [actions],
+            });
+
+            const filter = (interaction) =>
+                ['unmute', 'ban'].includes(interaction.customId) &&
+                interaction.member.roles.cache.has(modRole.id);
+
+            const collector = sentMsg.createMessageComponentCollector({ filter, time: 60000 });
+
+            collector.on('collect', async (interaction) => {
+                if (interaction.customId === 'unmute') {
+                    await member.roles.remove(mutedRole);
+                    await interaction.reply({ content: `Le bot <@${member.user.id}> a été unmute.`, ephemeral: true });
+                } else if (interaction.customId === 'ban') {
+                    await member.ban({ reason: "Bot threat - action par modérateur" });
+                    await interaction.reply({ content: `Le bot <@${member.user.id}> a été banni.`, ephemeral: true });
+                }
+            });
+        }
+    }
+};
+
 module.exports = {
     name: Events.GuildMemberAdd,
     async execute(client, guildMember) {
+        handleBotThreats(guildMember);
         const channel = guildMember.guild.channels.cache.get(
             jsonConfig.welcome_channel
         );
@@ -30,7 +143,7 @@ module.exports = {
                 name: embedDataMP.author.name,
                 url:
                     embedDataMP.author.url || "https://www.ecole-directe.plus/",
-                iconURL: embedDataMP.author.iconUrl,
+                iconURL: embedDataMP.author.icon_url,
             });
         const embedData = JSON.parse(
             fs.readFileSync(
